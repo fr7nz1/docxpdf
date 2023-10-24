ec7409c18d5bc75077d3c54c2eec21e7ef88ea09
ka1ssu/Шляхтин Роман <124870566+ka1ssu@users.noreply.github.com>
2023-10-24 15:28:09 +0600

    Add files via upload

--- table.py
+++ table.py
@@ -1,53 +1,55 @@
 import docx
 
 doc = docx.Document('1.docx')
-tablescount = 0
 
-# Пройдите по всем таблицам в документе
-for table in doc.tables[1:]:
-    # Выполните некоторые действия с таблицей...
-    tablescount += 1
-    pass
 
-linktablescount = 0
-i = 1
-mask_template = """Таблица {chislo} —""" #Задаем маску для последующей проверки по ней
-mask = mask_template.format(chislo=i) #Добавляет переменную в маску
+def search_tables(file):
+    tablescount = 0
+    linktablescount = 0
+    i = 1
+    # Пройдите по всем таблицам в документе
+    for table in doc.tables[1:]:
+        # Выполните некоторые действия с таблицей...
+        tablescount += 1
+        pass
 
-for paragraph in doc.paragraphs: #Проходит по всем абзацам
-    text = paragraph.text #Записывает текст всего абзаца в переменную
-    if mask in text: #Проверяет есть ли маска в тексте
-        linktablescount += 1
-        i += 1
-        mask = mask_template.format(chislo=i)
-    text = ''
+    mask_template = """Таблица {chislo} —"""  # Задаем маску для последующей проверки по ней
+    mask = mask_template.format(chislo=i)  # Добавляет переменную в маску
+    for paragraph in doc.paragraphs:  # Проходит по всем абзацам
+        text = paragraph.text  # Записывает текст всего абзаца в переменную
+        if mask in text:  # Проверяет есть ли маска в тексте
+            linktablescount += 1
+            i += 1
+            mask = mask_template.format(chislo=i)
+        text = ''
+    print("Количество таблиц:", tablescount)
+    print("Количество ссылок на таблицы:", linktablescount)
 
-piccount = 0
-linkpiccount = 0
-j = 1
 
-#Проходит по всем иллюстрациям в документе
-for shape in doc.inline_shapes:
-    piccount += 1
-    pass
+def search_pic(file):
+    piccount = 0
+    linkpiccount = 0
+    j = 1
+    # Проходит по всем иллюстрациям в документе
+    for shape in doc.inline_shapes:
+        piccount += 1
+        pass
 
-mask_template_pic = """Рисунок {chislo} —""" #Задаем маску для последующей проверки по ней
-mask_pic = mask_template_pic.format(chislo=j) #Добавляет переменную в маску
+    mask_template_pic = """Рисунок {chislo} —"""  # Задаем маску для последующей проверки по ней
+    mask_pic = mask_template_pic.format(chislo=j)  # Добавляет переменную в маску
 
-for paragraph in doc.paragraphs: #Проходит по всем абзацам
-    text = paragraph.text #Записывает текст всего абзаца в переменную
-    if mask_pic in text: #Проверяет есть ли маска в тексте
-        linkpiccount += 1
-        j += 1
-        mask_pic = mask_template_pic.format(chislo=j)
-    text = ''
+    for paragraph in doc.paragraphs:  # Проходит по всем абзацам
+        text = paragraph.text  # Записывает текст всего абзаца в переменную
+        if mask_pic in text:  # Проверяет есть ли маска в тексте
+            linkpiccount += 1
+            j += 1
+            mask_pic = mask_template_pic.format(chislo=j)
+        text = ''
+    print("Количество иллюстраций:", piccount)
+    print("Количество ссылок на иллюстрации:", linkpiccount)
 
 # Вывести количество таблиц
-print("Количество таблиц(все):", len(doc.tables)) #Считает все таблицы + та что на титульном
-print("Количество ссылок на таблицы:", linktablescount)
-print("Количество таблиц:", tablescount)
-print("Количество ссылок на иллюстрации:", linkpiccount)
-print("Количество иллюстраций:", piccount)
+#print("Количество таблиц(все):", len(doc.tables)) #Считает все таблицы + та что на титульном
 
 # Не работает, надо чета делать :(
 def check_links():
